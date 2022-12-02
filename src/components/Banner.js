b1e6a121f57ee3e4c328c6a24a37dd03c71ef195
Kaspervdlaan <kaspervdlaan@gmail.com>
2022-12-02 16:20:16 +0100

    fixing bg-sizing

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,7 +5,7 @@ import Footer from "./Footer";
 function Banner({ title, image, footer }) {
     return (
         <div className="md:overflow-hidden md:max-h-[100vh] md:h-auto h-screen relative">
-            <img className="h-full object-cover" src={image} alt=""/>
+            <img className="h-full w-full object-cover" src={image} alt=""/>
             <div className="absolute bottom-8 right-0 text-right p-4 max-w-3xl ">
                 <Link to=""><h2 className="text-xl font-normal underline text-blue-700 2xl:text-2xl">{title}</h2></Link>
             </div>
